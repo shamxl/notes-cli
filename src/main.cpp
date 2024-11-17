b809f97eb7e4a35766580acaced8bbebe166b1d7
shamxl <shamxl@pm.me>
2024-11-17 19:37:07 +0530

    Refactor: use new functions
    Add: write to note functionality
    Add: note selection

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <argparse/argparse.hpp>
-#include "config.h"
 #include "parser.h"
+#include "helpers.h"
 
 int main (int argc, char* argv[]) {
   argparse::ArgumentParser cli ("Notes");
   init_arguments (cli);
-  Config config;
 
   try {
     cli.parse_args(argc, argv);
@@ -14,33 +13,62 @@ int main (int argc, char* argv[]) {
     std::cout << cli << std::endl;
     std::exit(1);
   }
+  
+  
+  Helpers::load_config();
+  
 
-  std::string command = cli.get<std::string>("command");
-
-  if (cli["--new"] == true) {
+  if (auto value = cli.present("-ng")) {
+    std::cout << (Helpers::create_group(*value) ? "Group created: " : "Group already exists: ") << *value << std::endl; 
+  } else if (auto value = cli.present ("-sg")) {
+    std::cout << (Helpers::select_group(*value) ? "Selected group: " : "Group doesn't exists: ") << *value << std::endl;
+  } else if (auto value = cli.present("-nn")) {
+    std::string selected_group = Helpers::get_selected_group();
+    if (selected_group.empty()) {
+      std::cout << "Select a group first!" << std::endl;
+    } 
     
-    if (cli["--group"] == true) {
-      config.add_group(command);
+    if (Helpers::create_note(*value, selected_group)) {
+      std::cout << "Created note \"" << *value << "\" in group \"" << selected_group << "\"" << std::endl;
+    } else {
+      std::cout << "Note \"" << *value << "\" already exists in group \"" << selected_group << "\"" << std::endl;
     }
-    if (cli["--note"] == true) {
-      config.add_note(command);
+    
+  } else if (auto value = cli.present ("-sn")) {
+    std::string selected_group = Helpers::get_selected_group();
+    if (selected_group.empty()) {
+      std::cout << "Select a group first!" << std::endl;
+      std::exit (1);
     }
+    bool select_status = Helpers::select_note(*value, selected_group);
+    std::cout << (select_status ? "Selected note: " : "Note doesn't exists: ") << *value << std::endl;
+  } else {
+    try {
+      auto notes = cli.get<std::vector<std::string>>("notes");
+      std::string selected_group = Helpers::get_selected_group();
+      if (selected_group.empty()) {
+        std::cout << "Select a group first!" << std::endl;
+        std::exit(1);
+      }
+      std::string selected_note = Helpers::get_selected_note(selected_group);
+      if (selected_note.empty()) {
+        std::cout << "No note selected in group: " << selected_group << std::endl;
+        std::exit(1);
+      }
 
-  } else if (cli["--select"] == true) {
+      bool write_status = Helpers::write_to_note(selected_note, selected_group, notes);
 
-    if (cli["--note"] == true) {
-      config.select_note(command);
+      if (!write_status) {
+        std::cout << "Writing failed." << std::endl;
+      } else {
+        std::cout << "Wrote to:" << std::endl;
+        std::cout << "  note: " << selected_note << std::endl;
+        std::cout << "  group: " << selected_group << std::endl;
+      }
+    } catch (nlohmann::json::exception& _e) {
+      cli.help();
     }
-    if (cli["--group"] == true) {
-      config.select_group(command);
-    }
-
-  } else if (cli["--group"] == true) {
-      config.getselected_group();
-  } else if (cli["--note"] == true) {
-    std::cout << "No note selected" << std::endl;
-  } else {}
-
+  }
 
   return 0;
 }
