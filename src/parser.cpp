42e65d6b422c4792cd22d47bd70b35c393765091
shamxl <shamxl@pm.me>
2024-11-17 19:33:18 +0530

    Rename argument name

--- src/parser.cpp
+++ src/parser.cpp
@@ -7,5 +7,5 @@ void init_arguments (argparse::ArgumentParser & cli) {
   cli.add_argument ("--new-group", "-ng");
   cli.add_argument ("--select-group", "-sg");
   cli.add_argument ("--select-note", "-sn");
-  cli.add_argument ("note").remaining();
+  cli.add_argument ("notes").remaining();
 }
