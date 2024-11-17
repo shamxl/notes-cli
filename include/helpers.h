f0bce3190e9de7817d0e5ff094e98533878d1a04
shamxl <shamxl@pm.me>
2024-11-17 19:35:25 +0530

    Add: new namespace for easy management of major functions

---
+++ include/helpers.h
@@ -0,0 +1,17 @@
+#ifndef NOTES_HELPERS_H
+#define NOTES_HELPERS_H
+
+#include <nlohmann/json.hpp>
+namespace  Helpers {
+  void load_config ();
+  void save_config ();
+  bool create_group (std::string name);
+  bool select_group (std::string name);
+  bool create_note (std::string name, std::string group_name);
+  bool select_note (std::string name, std::string group_name);
+  bool write_to_note (std::string name, std::string group_name, std::vector<std::string> &notes);
+  std::string get_selected_group ();
+  std::string get_selected_note (std::string group_name);
+}
+
+#endif
---
+++ src/utils/helpers.cpp
@@ -0,0 +1,128 @@
+#include "helpers.h"
+#include "config.h"
+#include <filesystem>
+#include <fstream>
+#include <nlohmann/json.hpp>
+#include <iostream>
+
+namespace Helpers {
+
+  nlohmann::json data;
+  Config config;
+  void load_config () {
+    std::ifstream ifile (config.indexFile);
+    data = nlohmann::json::parse(ifile);
+    if (!data.contains("groups")) {
+      data["groups"] = nlohmann::json::object();
+    }
+  }
+
+  void save_config () {
+    std::ofstream ofile(config.indexFile);
+    if (!ofile.is_open()) {
+      std::cerr << "Failed to save config file" << std::endl;
+    }
+    ofile << data.dump(2);
+    ofile.close();
+  }
+
+  bool create_group (std::string name) {
+    if (data["groups"].contains(name)) {
+      return false;
+    }
+
+    std::string group_path = std::string (config.groupDir) + "/" + name;
+
+    data["groups"][name] = {
+      {"path", group_path},
+      {"notes", nlohmann::json::object()}
+    };
+
+    if (!fs::create_directory(group_path)) {
+      return false;
+    }
+
+    save_config();
+    return true;
+  }
+
+  bool select_group (std::string name) {
+    if (!data["groups"].contains(name)) {
+      return false;
+    } 
+    data["selected_group"] = name;
+    save_config();
+    return true;
+  }
+
+  bool create_note (std::string name, std::string group_name) {
+    nlohmann::json& cur_group = data["groups"][group_name];
+    nlohmann::json& notes = cur_group["notes"];
+
+    if (notes.contains(name)) {
+      return false;
+    }
+
+    if (notes.contains(name)) {
+      return false;
+    }
+
+    std::string note_path = std::string (cur_group["path"]) + "/" + name + ".txt";
+    
+
+    std::ofstream ofile (note_path);
+    ofile.close();
+    
+    notes[name] = note_path;
+
+    save_config ();
+    return true;
+  }
+
+  bool select_note (std::string name, std::string group_name) {
+    if (!data["groups"][group_name]["notes"].contains(name)) {
+      return false;
+    }
+    data["groups"][group_name]["selected_note"] = name;
+    save_config ();
+    return true;
+  }
+
+  bool write_to_note(std::string name, std::string group_name, std::vector<std::string> &notes) {
+    nlohmann::json& cur_group = data["groups"][group_name];
+    nlohmann::json& cur_note = cur_group["notes"][name];
+
+    std::ofstream ofile((std::string) cur_note, std::ios::app);
+    if (ofile.is_open()) {
+      for (const auto& val : notes) {
+        ofile << val << " ";
+      }
+
+      ofile << "\n";
+    } else {
+      return false;
+    }
+
+    ofile.close();
+    return true;
+  }
+
+  std::string get_selected_group () {
+    if (!data.contains("selected_group")) {
+      return std::string();
+    }
+
+    return data["selected_group"];
+  }
+
+  std::string get_selected_note (std::string group_name) {
+    nlohmann::json cur_group = data["groups"][group_name];
+    if (!cur_group.contains("selected_note")) {
+      return std::string();
+    } 
+    
+    return cur_group["selected_note"];
+  }
+}
+
+
