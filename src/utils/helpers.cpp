81a2d843c8e049ee6aba8918f2b1cc01ec2360b0
shamxl <shamxl@pm.me>
2024-11-23 14:46:53 +0530

    Fix: notes and groups deletion failing

--- src/utils/helpers.cpp
+++ src/utils/helpers.cpp
@@ -135,31 +135,31 @@ namespace Helpers {
 
   bool delete_group(std::string name) {
     
-    nlohmann::json& group = data["groups"];
-    if (!group.contains(name)) {
+    nlohmann::json& groups = data["groups"];
+    if (!groups.contains(name)) {
       return false;
     } 
     
-    nlohmann::json& cur_group = data[name];
+    nlohmann::json& cur_group = groups[name];
     fs::remove_all (cur_group["path"]);
-    data["groups"].erase (name);
+    groups.erase (name);
     save_config ();
     return true;
   }
 
   bool delete_note(std::string group_name, std::string note_name) {
-    nlohmann::json& group = data["groups"];
-    if (!group.contains(group_name)) {
+    nlohmann::json& groups = data["groups"];
+    if (!groups.contains(group_name)) {
       return false;
     }
 
-    nlohmann::json& notes = group["notes"];
+    nlohmann::json& notes = groups[group_name]["notes"];
     if (!notes.contains(note_name)) {
       return false;
     }
 
-    notes.erase(note_name);
     std::string note_path = get_note_path(group_name, note_name);
+    notes.erase(note_name);
     fs::remove (note_path);
     save_config();
     return true;
